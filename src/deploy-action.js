577b688fc0d95a2551a59f11d7a1e47b1661ff9b
Rex Ledesma <rex@elementl.com>
2021-11-15 06:52:09 -0800

    feat: add code preview action

--- src/deploy-action.js
+++ src/deploy-action.js
@@ -1,46 +1,8 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 const github = require("@actions/github");
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
-const util = require("util");
-const YAML = require("yaml");
 const { DagsterCloudClient } = require("./dagsterCloud");
-
-const writeFileAsync = util.promisify(fs.writeFile);
-
-async function inParallel(locations, processingFunction) {
-  await Promise.all(Object.entries(locations).map(processingFunction));
-}
-
-async function inSeries(locations, processingFunction) {
-  for (const location of locations) {
-    await processingFunction(location);
-  }
-}
-
-function tmpDir() {
-  return fs.mkdtempSync(path.join(os.tmpdir(), "dagster-cloud-ci"));
-}
-
-async function writeRequirementsDockerfile(baseImage) {
-  const dockerfilePath = path.join(tmpDir(), "Dockerfile");
-  await writeFileAsync(
-    dockerfilePath,
-    `
-FROM ${baseImage}
-
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-
-WORKDIR /opt/dagster/app
-
-COPY . /opt/dagster/app
-  `
-  );
-  return dockerfilePath;
-}
+const { getProcess, getLocations, buildDockerImages } = require("./utils");
 
 async function run() {
   try {
@@ -49,71 +11,12 @@ async function run() {
 
     const locationFile = core.getInput("location-file");
 
-    const locations = await core
-      .group("Read locations.yaml", async () => {
-        const locationsFile = fs.readFileSync(locationFile, "utf8");
-        return YAML.parse(locationsFile).locations;
-      })
-      .catch((error) => {
-        core.error(`Error reading locations.yaml: ${error}`, {
-          file: locations,
-        });
-      });
+    const locations = await getLocations(locationFile);
 
     const parallel = core.getBooleanInput("parallel");
-    const process = parallel ? inParallel : inSeries;
+    const process = getProcess(parallel);
 
-    await core.group("Build Docker images", async () => {
-      await process(locations, async ([_, location]) => {
-        const basePath = path.parse(locationFile).dir;
-        const buildPath = path.join(basePath, location["build"]);
-
-        let dockerfile = path.join(buildPath, "Dockerfile");
-        const baseImage = location["base_image"];
-
-        if (!fs.existsSync(dockerfile)) {
-          const requirementsFile = path.join(buildPath, "requirements.txt");
-
-          if (!fs.existsSync(requirementsFile) || !baseImage) {
-            core.error(
-              "Supplied build path must either contain Dockerfile, or requirements.txt with base_image"
-            );
-          }
-
-          dockerfile = await writeRequirementsDockerfile(baseImage);
-        } else {
-          if (baseImage) {
-            core.error(
-              "No need to specify base_image for location if build path contains Dockerfile"
-            );
-          }
-
-          dockerfile = "./Dockerfile";
-        }
-
-        const imageName = `${location["registry"]}:${imageTag}`;
-
-        let dockerArguments = [
-          "build",
-          ".",
-          "--label",
-          `sha=${github.context.sha}`,
-          "-f",
-          dockerfile,
-          "-t",
-          imageName,
-        ];
-
-        if (location["target"]) {
-          dockerArguments = dockerArguments.concat([
-            "--target",
-            location["target"],
-          ]);
-        }
-
-        await exec.exec("docker", dockerArguments, { cwd: buildPath });
-      });
-    });
+    await buildDockerImages(process, locationFile, locations, imageTag);
 
     await core.group("Push Docker image", async () => {
       await process(locations, async ([_, location]) => {

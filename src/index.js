392b2c53ae48d71d7986747b6259fb90dac337f6
Max Gasner <max.gasner@gmail.com>
2021-11-18 23:12:00 -0800

    Hack to get env variables into ECS

--- src/index.js
+++ src/index.js
@@ -86,6 +86,10 @@ async function run() {
             'build', '.',
             '--label', `sha=${github.context.sha}`,
             '-f', dockerfile,
+            // Brutal hack to get secrets into a container for ECS
+            '--build-arg', `SNOWFLAKE_USER=${core.getInput("SNOWFLAKE_USER")}`,
+            '--build-arg', `SNOWFLAKE_ACCOUNT=${core.getInput("SNOWFLAKE_ACCOUNT")}`,
+            '--build-arg', `SNOWFLAKE_PASSWORD=${core.getInput("SNOWFLAKE_PASSWORD")}`,
             '-t', imageName
           ],
           options = {'cwd': buildPath}
